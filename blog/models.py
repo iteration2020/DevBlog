d87712993459d6d74ff6ea1c81f6ff4f6afa36d2
iteration2020 <ar.antipov@gmail.com>
2023-10-01 17:11:41 +0300

    add INSTALLED_APPS info

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-class Post (models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=450)
     author = models.ForeignKey(
         'auth.User',
