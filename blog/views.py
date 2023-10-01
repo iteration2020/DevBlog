e45ac8a4864d5ce7a4fb338da3caf0f10cd7c0a4
iteration2020 <ar.antipov@gmail.com>
2023-10-01 18:40:45 +0300

    add .html files, progress homepage structure with bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Post
 
-# Create your views here.
+
+class BlogList(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'post_detail.html'
