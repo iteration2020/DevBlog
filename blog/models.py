c3639696762d06c4a7e08f2a2e085cd576db654f
anna <koychieva.anna@mail.ru>
2023-10-05 20:42:39 +0300

    aaa

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-
-# Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=450)
-    author = models.ForeignKey(
-        'auth.User',
-        on_delete=models.CASCADE,
-    )
-    body = models.TextField()
+    title = models.CharField(max_length=450) # заголовок поста
+    author = models.ForeignKey('auth.User', # Автор поста, которого выбираем в административной панели управления
+                               on_delete=models.CASCADE) # Удаление поста
+    body = models.TextField(null=True, blank=True) # Поле нашего поста
 
     def __str__(self):
         return self.title
