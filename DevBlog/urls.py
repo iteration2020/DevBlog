a77ef4683d1f3c02e2f08cf08d27081035a7fb12
anna <koychieva.anna@mail.ru>
2023-10-08 14:45:54 +0300

    Скорректировала рамочку на авторизации

--- DevBlog/urls.py
+++ DevBlog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('login/', LoginView.as_view(template_name='autorise.html'), name='login'),
 ]
