9d88538ba05a06debdb24806ab5ad752b52c5255
anna <koychieva.anna@mail.ru>
2023-10-08 17:28:09 +0300

    Хренова туча изменений: -Обновила основную страницу сайта -Новая папка (юзерс, куда кладу html и css, но не всегда) -Переход на авторизацию
    И еще куча всего чего я уже забыла

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=450) # заголовок поста
+    title = models.CharField(max_length=100) # заголовок поста
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey('auth.User', # Автор поста, которого выбираем в административной панели управления
                                on_delete=models.CASCADE) # Удаление поста
     body = models.TextField(null=True, blank=True) # Поле нашего поста
