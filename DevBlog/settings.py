fa615ca5d613f821d07e3f25b5d9f6ff71b2ddae
anna <koychieva.anna@mail.ru>
2023-10-09 20:24:31 +0300

    Починила регистрацию

--- DevBlog/settings.py
+++ DevBlog/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'crispy_bootstrap4'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
