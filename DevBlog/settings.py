818dadd5fb3b5db8f500961f734a8d3b47727dd5
anna <koychieva.anna@mail.ru>
2023-10-12 20:18:56 +0300

    Корректировочные правки

--- DevBlog/settings.py
+++ DevBlog/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'DevBlog',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
